a0b5d8a463f7fc9dfd98c592e751943abf2ec841
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-11-22 14:35:28 +0500

    failed rescript attempt, freshened in flavor of rust

--- nix-language-parser/src/examples/fetchgit.nix
+++
@@ -1,9 +0,0 @@
-# https://zero-to-nix.com/concepts/reproducibility
-{ stdenv, fetchGit }:
-stdenv.mkDerivation {
-  name = "my-package";
-  src = fetchgit {
-    url = "https://github.com/DeterminateSystems/riff";
-    sha256 = "sha256-7mnx7J0AacL2P2mNuNNB+kKE7VR8nniVG+PSrwpZixE=";
-  };
-}
--- todo.md
+++
@@ -1,3 +0,0 @@
-abstract:
-- [ ] lookup github:NixOS/nixpkgs for various revisions and generate full package meta via [simple-git](https://www.npmjs.com/package/simple-git)
-- [ ] write nix language parser for parsing these files in order to get metadata from nix files (and for future ofc)
