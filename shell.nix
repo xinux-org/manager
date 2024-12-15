3d75d8e0349aeb5de028a7c459e0b8800182d179
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-12-15 22:00:23 +0500

    chore: proper shell

--- shell.nix
+++ shell.nix
@@ -2,14 +2,24 @@ let
   rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
   nixpkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
   rust_channel = nixpkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-  getLibFolder = pkg: "${pkg}/lib";
-  libiconvPath = "${nixpkgs.libiconv}/lib";
 in
 with nixpkgs;
 
 pkgs.mkShell {
+  nativeBuildInputs = [pkg-config] ++ (with pkgs; [
+    openssl
+    libgit2
+  ]);
+
   buildInputs = (with pkgs; [
     flutter
     android-tools
   ]);
+
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+    pkgs.openssl
+    pkgs.libgit2
+  ];
+  
+  RUST_SRC_PATH = "${rust_channel}/lib/rustlib/src/rust/library";
 }
