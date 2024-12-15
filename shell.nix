e4ad344a04dce3f2d6ede397687913e93142aa9f
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-12-15 21:01:51 +0500

    feat: flutter init

--- shell.nix
+++ shell.nix
@@ -1,61 +1,15 @@
-{ pkgs ? import <nixpkgs> { } }:
 let
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  nixpkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  rust_channel = nixpkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
   getLibFolder = pkg: "${pkg}/lib";
-  getFramwork = pkg: "${pkg}/Library/Frameworks";
-  darwinOptions =
-    if pkgs.stdenv.isDarwin then ''
-      -F${(getFramwork pkgs.darwin.apple_sdk.frameworks.Security)}
-      -F${(getFramwork pkgs.darwin.apple_sdk.frameworks.CoreFoundation)}
-      -F${(getFramwork pkgs.darwin.apple_sdk.frameworks.CoreServices)}
-      -F${(getFramwork pkgs.darwin.apple_sdk.frameworks.SystemConfiguration)}
-    '' else "";
+  libiconvPath = "${nixpkgs.libiconv}/lib";
 in
-pkgs.stdenv.mkDerivation {
-  name = "nix-monorepo";
+with nixpkgs;
 
-  nativeBuildInputs = with pkgs; [
-    # LLVM & GCC
-    gcc
-    cmake
-    gnumake
-    pkg-config
-    llvmPackages.llvm
-    llvmPackages.clang
-
-    # Hail the Nix
-    nixd
-    nixpkgs-fmt
-
-    # Launch scripts
-    just
-
-    # Rust
-    rustc
-    cargo
-    clippy
-    cargo-watch
-    rust-analyzer
-  ];
-
-  # Having hard times nix running from macOS 15 Beta?
-  # add these to your buildInputs:
-  # darwin.apple_sdk.frameworks.Security
-  # darwin.apple_sdk.frameworks.CoreServices
-  # darwin.apple_sdk.frameworks.CoreFoundation
-  # darwin.apple_sdk.frameworks.SystemConfiguration
-  buildInputs = with pkgs; [
-    openssl
-    pkg-config
-    sqlite
-  ];
-
-  # Set Environment Variables
-  RUST_BACKTRACE = 1;
-  NIX_LDFLAGS = "-L${(getLibFolder pkgs.libiconv)} ${darwinOptions}";
-  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-    (getLibFolder pkgs.gcc)
-    (getLibFolder pkgs.libiconv)
-    (getLibFolder pkgs.llvmPackages.llvm)
-  ];
+pkgs.mkShell {
+  buildInputs = (with pkgs; [
+    flutter
+    android-tools
+  ]);
 }
