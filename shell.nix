da768dd45dad0fb66dfaf07969c797d230457a3e
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-12-23 21:31:49 +0500

    feat: rinf is working

--- shell.nix
+++ shell.nix
@@ -10,22 +10,29 @@ pkgs.mkShell {
     openssl
     libgit2
     sqlite
+    protobuf
   ]);
 
   buildInputs = (with pkgs; [
     flutter
     android-tools
     pandoc
+    rustup
   ]);
 
   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
     pkgs.openssl
     pkgs.libgit2
     pkgs.sqlite
+    pkgs.protobuf
   ];
   
   RUST_SRC_PATH = "${rust_channel}/lib/rustlib/src/rust/library";
   LINK_MANPAGES_PANDOC_FILTER = import ./nixos-search/flake-info/src/data/link-manpages.nix { inherit pkgs; };
+  FLUTTER_ROOT = "${pkgs.flutter}";
+
+  LC_CTYPE="en_US.UTF-8";
+  LC_ALL="en_US.UTF-8";
 
   shellHook = ''
     export PATH="$PATH:${rust_channel}/bin"
