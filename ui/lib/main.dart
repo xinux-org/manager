fc67c80b28faa11e4b717a37645f2bb79326d163
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-12-23 22:56:22 +0500

    feat: flutter basic ui

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:rinf/rinf.dart';
 import './messages/all.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   await initializeRust(assignRustSignal);
@@ -13,43 +15,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    final theme = ThemeData(
+      brightness: Brightness.dark,
+      colorScheme: const ColorScheme.highContrastDark(
+        primary: Color.fromRGBO(105, 154, 215, 1),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      useMaterial3: true,
+    );
+
+    return MaterialApp(
+      title: 'Xinux Manager',
+      themeMode: ThemeMode.dark,
+      darkTheme: theme.copyWith(
+        textTheme: GoogleFonts.interTextTheme(theme.textTheme),
+      ),
+      home: const MyHomePage(title: 'Xinux Manager'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -58,71 +44,123 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        title: Row(
+          children: [
+            SvgPicture.asset(
+              "assets/xinux-logo.svg",
+              height: 32,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(width: 12),
+            Text(widget.title),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 60,
+                  width: 320,
+                  padding: const EdgeInsets.only(top: 12),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Search",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 12),
+            SizedBox(
+              width: double.infinity,
+              child: DataTable(
+                columns: const [
+                  DataColumn(
+                    label: Expanded(
+                      child: Text("Name"),
+                    ),
+                  ),
+                  DataColumn(
+                    label: Text("Type"),
+                  ),
+                  DataColumn(
+                    label: Text("Broken"),
+                  ),
+                  DataColumn(
+                    label: Text("Insecure"),
+                  ),
+                  DataColumn(
+                    label: Text("Unfree"),
+                  ),
+                  DataColumn(
+                    label: Text("Unsupported"),
+                  ),
+                ],
+                rows: [
+                  DataRow(
+                    cells: [
+                      const DataCell(
+                        Text("firefox"),
+                      ),
+                      const DataCell(
+                        Tooltip(
+                          message: "Package",
+                          child: Icon(Icons.inventory),
+                        ),
+                      ),
+                      DataCell(
+                        Checkbox(
+                          value: false,
+                          onChanged: (bool? value) {},
+                        ),
+                      ),
+                      DataCell(
+                        Checkbox(
+                          value: false,
+                          onChanged: (bool? value) {},
+                        ),
+                      ),
+                      DataCell(
+                        Checkbox(
+                          value: false,
+                          onChanged: (bool? value) {},
+                        ),
+                      ),
+                      DataCell(
+                        Checkbox(
+                          value: false,
+                          onChanged: (bool? value) {},
+                        ),
+                      ),
+                    ],
+                  ),
+                  // const DataRow(
+                  //   cells: [
+                  //     DataCell(
+                  //       Text("programs.firefox.enable"),
+                  //     ),
+                  //     DataCell(
+                  //       Tooltip(
+                  //         message: "Option",
+                  //         child: Icon(Icons.settings),
+                  //       ),
+                  //     ),
+                  //   ],
+                  // ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
